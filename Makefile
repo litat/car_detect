8e7c7aa6d3d06afa8efd113f2553e1be6d24c60f
Lita Chang <fireice47@gmail.com>
2015-12-14 08:44:02 +0800

    finally detect car using input video.

--- Makefile
+++ Makefile
@@ -1,11 +1,9 @@
 CXX = g++
 
 CXXFLAGS = -c -Wall $(shell \
-	pkg-config --cflags opencv \
-	pkg-config --cflags opencv3)
+	pkg-config --cflags opencv)
 LDFLAGS = $(shell \
-	pkg-config --libs opencv \
-	pkg-config --libs opencv3)
+	pkg-config --libs opencv)
 
 TARGET_NAME = car_detect
 SRC_DIR = .
@@ -18,19 +16,18 @@ objects = $(sources:$(SRC_DIR)/%.cpp=$(BIN_DIR)/%.o)
 all: $(BIN_DIR) $(EXECUTABLE)
 
 $(BIN_DIR):
-	mkdir -p $(BIN_DIR)
+	@mkdir -p $(BIN_DIR)
+	@echo $(BIN_DIR) " created."
 
 $(EXECUTABLE): $(objects)
-	$(CXX) $< -o $@ $(LDFLAGS)
+	@$(CXX) $< -o $@ $(LDFLAGS)
+	@echo "Link " $< "successfully."
 
 $(BIN_DIR)/%.o: $(SRC_DIR)/%.cpp
-	$(CXX) $< -o $@ $(CXXFLAGS)
+	@$(CXX) $< -o $@ $(CXXFLAGS)
+	@echo "Compile " $< "successfully."
 
-.PHONY: clean print
+.PHONY: clean
 
 clean:
 	rm -f *.o $(EXECUTABLE)
-
-print:
-	echo $(sources)
-	echo $(objects)
--- car_detect.cpp
+++ car_detect.cpp
@@ -8,7 +8,7 @@
 
 This code provides faster car detection.
 
-Also for the first time multiple cascade files are used to detect objects,with a benefit that no two objects
+Also for the first time multiple cascade files are used to detect objects,  with a benefit that no two objects
 are detected twice.
 
 Ever car detected in an image goes through a two stage testing.
@@ -38,9 +38,6 @@ void help()
 	cout << endl << "ckeckcas.xml is the one trained with smallest size parameters and the rest are the main cascades" << endl;
 }
 
-
-
-
 class cars     //main class
 {
 	public:	    //variables kept public but precaution taken all over the code
@@ -50,27 +47,26 @@ class cars     //main class
 	Mat storage;              //introduced to stop detection of same car more than once
 
 	CascadeClassifier cascade;    //the main cascade classifier
-	CascadeClassifier checkcascade;        //a test classifier,car detected by both main and test is stated as car
+	CascadeClassifier checkcascade;        //a test classifier,  car detected by both main and test is stated as car
 
 	int num;
 
 	void getimage(Mat src) //getting the input image
-    {
+	{
 
-        if(! src.data )
-        {
-            cout <<  "src not filled" << endl ;
-        }
-
-        else
-        {
-            image_input = src.clone();
+		if(! src.data )
+		{
+			cout <<  "src not filled" << endl ;
+		}
+		else
+		{
+			image_input = src.clone();
 			storage = src.clone();              //initialising storage
 			image_main_result = src.clone();    //initialising result
 
-            cout << "got image" <<endl;
-        }
-    }
+			// cout << "got image" <<endl;
+		}
+	}
 
 
 	void cascade_load(string cascade_string)            //loading the main cascade
@@ -78,16 +74,13 @@ class cars     //main class
 		cascade.load(cascade_string);
 
 		if( !cascade.load(cascade_string) )
-    	{
-        	cout << endl << "Could not load classifier cascade" << endl;
-
-    	}
+		{
+			cout << endl << "Could not load classifier cascade" << endl;
+		}
 		else
 		{
-
 			cout << "cascade : " << cascade_string << " loaded" << endl;
 		}
-
 	}
 
 
@@ -96,10 +89,9 @@ class cars     //main class
 		checkcascade.load(checkcascade_string);
 
 		if( !checkcascade.load(checkcascade_string) )
-    	{
-        	cout << endl << "Could not load classifier checkcascade" << endl;
-
-    	}
+		{
+			cout << endl << "Could not load classifier checkcascade" << endl;
+		}
 		else
 		{
 			cout<< "checkcascade : " << checkcascade_string << " loaded" << endl;
@@ -110,18 +102,17 @@ class cars     //main class
 	void display_input()             // function to display input
 	{
 		namedWindow("display_input");
-		imshow("display_input",image_input);
-		waitKey(0);
+		imshow("display_input", image_input);
+		// waitKey(0);
 	}
 
 	void display_output()            //function to display output
 	{
-
 		if(!image_main_result.empty() )
-        {
+		{
 			namedWindow("display_output");
-			imshow("display_output",image_main_result);
-			waitKey(0);
+			imshow("display_output", image_main_result);
+			// waitKey(0);
 		}
 	}
 
@@ -133,40 +124,38 @@ class cars     //main class
 
 	void findcars()                 //main function
 	{
-    	int i = 0;
+		int i = 0;
 
 		Mat img = storage.clone();
-		Mat temp;                    //for region of interest.If a car is detected(after testing) by one classifier,then it will not be available for other one
+		Mat temp;                    //for region of interest.If a car is detected(after testing) by one classifier,  then it will not be available for other one
 
 		if(img.empty() )
-        {
+		{
 			cout << endl << "detect not successful" << endl;
 		}
+
 		int cen_x;
 		int cen_y;
-    	vector<Rect> cars;
-    	const static Scalar colors[] =  { CV_RGB(0,0,255),CV_RGB(0,255,0),CV_RGB(255,0,0),CV_RGB(255,255,0),CV_RGB(255,0,255),CV_RGB(0,255,255),CV_RGB(255,255,255),CV_RGB(128,0,0),CV_RGB(0,128,0),CV_RGB(0,0,128),CV_RGB(128,128,128),CV_RGB(0,0,0)};
+		vector<Rect> cars;
+		const static Scalar colors[] =  { CV_RGB(0,  0,  255), CV_RGB(0,  255,  0), CV_RGB(255,  0,  0), CV_RGB(255,  255,  0), CV_RGB(255,  0,  255), CV_RGB(0,  255,  255), CV_RGB(255,  255,  255), CV_RGB(128,  0,  0), CV_RGB(0,  128,  0), CV_RGB(0,  0,  128), CV_RGB(128,  128,  128), CV_RGB(0,  0,  0)};
 
-    	Mat gray;
+		Mat gray;
 
-    	cvtColor( img, gray, CV_BGR2GRAY );
+		cvtColor( img,  gray,  CV_BGR2GRAY );
 
-		Mat resize_image(cvRound (img.rows), cvRound(img.cols), CV_8UC1 );
+		Mat resize_image(cvRound (img.rows),  cvRound(img.cols),  CV_8UC1 );
 
-    	resize( gray, resize_image, resize_image.size(), 0, 0, INTER_LINEAR );
-    	equalizeHist( resize_image, resize_image );
+		resize( gray,  resize_image,  resize_image.size(),  0,  0,  INTER_LINEAR );
+		equalizeHist( resize_image,  resize_image );
 
+		cascade.detectMultiScale( resize_image,  cars,  1.1,  2,  0,  Size(10,  10));                 //detection using main classifier
 
-    	cascade.detectMultiScale( resize_image, cars,1.1,2,0,Size(10,10));                 //detection using main classifier
-
-
-		for( vector<Rect>::const_iterator main = cars.begin(); main != cars.end(); main++, i++ )
-    	{
-       		Mat resize_image_reg_of_interest;
-        	vector<Rect> nestedcars;
-        	Point center;
-        	Scalar color = colors[i%8];
-
+		for( vector<Rect>::const_iterator main = cars.begin(); main != cars.end(); main++,  i++ )
+		{
+			Mat resize_image_reg_of_interest;
+			vector<Rect> nestedcars;
+			Point center;
+			Scalar color = colors[i%8];
 
 			//getting points for bouding a rectangle over the car detected by main
 			int x0 = cvRound(main->x);
@@ -174,99 +163,123 @@ class cars     //main class
 			int x1 = cvRound((main->x + main->width-1));
 			int y1 = cvRound((main->y + main->height-1));
 
+			if( checkcascade.empty() )
+				continue;
 
+			resize_image_reg_of_interest = resize_image(*main);
+			checkcascade.detectMultiScale( resize_image_reg_of_interest,  nestedcars,  1.1,  2,  0,  Size(30,  30));
 
-        	if( checkcascade.empty() )
-            	continue;
-        	resize_image_reg_of_interest = resize_image(*main);
-        	checkcascade.detectMultiScale( resize_image_reg_of_interest, nestedcars,1.1,2,0,Size(30,30));
-
-        	for( vector<Rect>::const_iterator sub = nestedcars.begin(); sub != nestedcars.end(); sub++ )      //testing the detected car by main using checkcascade
-        	{
-           		center.x = cvRound((main->x + sub->x + sub->width*0.5));        //getting center points for bouding a circle over the car detected by checkcascade
-				cen_x = center.x;
-			   	center.y = cvRound((main->y + sub->y + sub->height*0.5));
-				cen_y = center.y;
-				if(cen_x>(x0+15) && cen_x<(x1-15) && cen_y>(y0+15) && cen_y<(y1-15))         //if centre of bounding circle is inside the rectangle boundary over a threshold the the car is certified
+			for( vector<Rect>::const_iterator sub = nestedcars.begin(); sub != nestedcars.end(); sub++ )      //testing the detected car by main using checkcascade
 				{
+					center.x = cvRound((main->x + sub->x + sub->width*0.5));        //getting center points for bouding a circle over the car detected by checkcascade
+					cen_x = center.x;
+					center.y = cvRound((main->y + sub->y + sub->height*0.5));
+					cen_y = center.y;
+					if(cen_x>(x0+15) && cen_x<(x1-15) && cen_y>(y0+15) && cen_y<(y1-15))         //if centre of bounding circle is inside the rectangle boundary over a threshold the the car is certified
+					{
 
-					rectangle( image_main_result, cvPoint(x0,y0),
-                    	   		cvPoint(x1,y1),
-                     	  		color, 3, 8, 0);               //detecting boundary rectangle over the final result
+						rectangle( image_main_result,  cvPoint(x0,  y0),
+						          cvPoint(x1,  y1),
+															color,  3,  8,  0);               //detecting boundary rectangle over the final result
 
+						//masking the detected car to detect second car if present
 
+						Rect region_of_interest = Rect(x0,  y0,  x1-x0,  y1-y0);
+						temp = storage(region_of_interest);
+						temp = Scalar(255,  255,  255);
 
-					//masking the detected car to detect second car if present
-
-					Rect region_of_interest = Rect(x0, y0, x1-x0, y1-y0);
-					temp = storage(region_of_interest);
-					temp = Scalar(255,255,255);
-
-					num = num+1;     //num if number of cars detected
-
-				}
+						num = num+1;     //num if number of cars detected
+					}
 			}
-
 		}
 
-
-	if(image_main_result.empty() )
-    {
-		cout << endl << "result storage not successful" << endl;
+		if(image_main_result.empty() )
+		{
+			cout << endl << "result storage not successful" << endl;
+		}
 	}
-
-    }
-
 };
 
-
-
-
-int main( int argc, const char** argv )
+int main( int argc,  const char** argv )
 {
-
 	double t = 0;
-    t = (double)cvGetTickCount();              //starting timer
-    Mat image1 = imread(argv[1],1);
-	Mat image;
-	resize(image1,image,Size(300,150),0,0,INTER_LINEAR);        //resizing image to get best experimental results
+	t = (double)cvGetTickCount();              //starting timer
+
 	cars detectcars;                      //creating a object
 
+	// string checkcas = argv[2];
+	// detectcars.checkcascade_load(checkcas);      //load the test cascade
+	detectcars.checkcascade_load("./checkcas.xml");      //load the test cascade
 
-	string checkcas = argv[2];
-
-	detectcars.getimage(image);           //get the image
 	detectcars.setnum();                  //set number of cars detected as 0
-	detectcars.checkcascade_load(checkcas);      //load the test cascade
 
-	//Applying various cascades for a finer search.
-	if(argc > 3)
+	// Applying various cascades for a finer search.
+	string cascades[] = {"./cas1.xml", "./cas2.xml", "./cas3.xml", "./cas4.xml"};
+	for (int i = 0; i < 4; i++)
 	{
-		for(int i = 3;i<argc;i++)
+		string cas = cascades[i];
+		detectcars.cascade_load(cas);
+	}
+
+	// if(argc > 3)
+	// {
+	// 	for(int i = 3;i<argc;i++)
+	// 	{
+	// 		string cas = argv[i];
+	// 		detectcars.cascade_load(cas);
+	// 		detectcars.findcars();
+	// 	}
+	// }
+	// else
+	// {
+	// 	help();
+	// 	cout << endl << "Please provide atleast one main cascade xml file" << endl;
+	// }
+
+	// Mat image1 = imread(argv[1], 1);
+	// Mat image;
+	Mat image1, image;
+
+	String input_file_name = argv[1];
+	VideoCapture capture(input_file_name);
+	if (capture.isOpened())
+	{
+		cout << "Capture is opened" << endl;
+		for (;;)
 		{
-			string cas = argv[i];
-			detectcars.cascade_load(cas);
+			capture >> image1;
+			if (image1.empty())
+			{
+				break;
+			}
+
+			resize(image1,  image,  Size(640,  360), 0,  0,  INTER_LINEAR);        //resizing image to get best experimental results
+
+			detectcars.getimage(image);           //get the image
 			detectcars.findcars();
+
+			t = (double)cvGetTickCount() - t;       //stopping the timer
+
+			// if(detectcars.num!=0)
+			// {
+			// 	cout << endl << detectcars.num << " cars got detected in = " << t/((double)cvGetTickFrequency()*1000.) << " ms" << endl << endl;
+			// }
+			// else
+			// {
+			// 	cout << endl << "cars not found" << endl;
+			// }
+
+			detectcars.display_output();          //displaying the final result
+
+			if (waitKey(1) >= 0)
+			{
+				break;
+			}
 		}
-	}
-	else
-	{
-		help();
-		cout << endl << "Please provide atleast one main cascade xml file" << endl;
+	} else {
+		cout << "No capture" << endl;
 	}
 
-	t = (double)cvGetTickCount() - t;       //stopping the timer
-
-	if(detectcars.num!=0)
-	{
-		cout << endl << detectcars.num << " cars got detected in = " << t/((double)cvGetTickFrequency()*1000.) << " ms" << endl << endl;
-	}
-	else
-	{
-		cout << endl << "cars not found" << endl;
-	}
-
-	detectcars.display_output();          //displaying the final result
-
+	capture.release();
 	return 0;
 }
